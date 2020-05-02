4c4d093230a82980dd1923390a3bd3ae504e4e21
Agustin Solano <aguchosolano@gmail.com>
2020-05-02 15:46:52 -0300

    Continuo definicion de Memoria
    Continuo definicion de la clase Memoria

--- Source/Memoria.py
+++ Source/Memoria.py
@@ -1,43 +1,63 @@
 import time
 
 class Memoria:
-    def __init__(self, nombre = " ", link= " ", fecha = " ", tags = []):
-        self.nombre = nombre
-        self.link = link
-        self.fecha = fecha
-        self.tags = tags
+    def __init__(self):
+        self.nombre = " "
+        self.link = " "
+        self.fecha = " "
+        self.tags = []
+        self.datos_ok = False
         self.posible_tags = ("articulo", "software", "twitter", "persona", "laboratorio", "paper")
 
+
     def set_memory(self, nombre, link, fecha, tags):
-        self.nombre = nombre
-        self.link = link
-        self.fecha = fecha
-        self.tags = tags
-        return True
+
+        #primero chequeo que el tag este bien
+        if self.check_tags(tags):
+            self.nombre = nombre
+            self.link = link
+            self.fecha = fecha
+            self.tags = tags
+            self.datos_ok = True
+            output = True
+        else:
+            print("Alguno de los tags no esta permitido!")
+            output = False
+        return output
 
     def get_memoria(self):
         return [self.nombre, self.link, self.fecha, self.tags]
 
     def print_memoria(self):
-        print("Nombre: {}\nLink: {}\nFecha: {}\nTags: {}".
-              format(self.nombre, self.link, time.strftime('%Y-%m-%d', self.fecha), self.tags))
-        return True
+        if self.datos_ok:
+            print("Nombre: {}\nLink: {}\nFecha: {}\nTags: {}".
+                  format(self.nombre, self.link, time.strftime('%Y-%m-%d', self.fecha), self.tags))
+            output = True
+        else:
+            print("El objeto no contiene datos correctos")
+            output = False
+        return output
 
-    def check_tags:
+    def check_tags(self, tags):
+        #Chequeo si todos los tags pertenece a posible_tags
+        check = [self.posible_tags.__contains__(elem) for elem in tags]
+        output = all(check) #check if all elements are true
+        return output
         
 
 
 
-
-
-
-nombre = "articulo interesante sobre suenio"
+'''nombre = "articulo interesante sobre suenio"
 link = "www.articulo.com"
 fecha = time.localtime()
-tags = ["articulo", "ciencia"]
+tags = ["articulo", "twitter"]
 ##
-mem1 = Memoria(nombre, link, fecha, tags)
+mem1 = Memoria()
+mem1.set_memory(nombre, link, fecha, tags)
 mem1.print_memoria()
+
+print(mem1.check_tags(["ciencia", "articulo"]))
+print(mem1.check_tags(["articulo", "twitter"]))'''
 '''print(nombre)
 print(link)
 print(time.strftime('%Y-%m-%d', fecha))
